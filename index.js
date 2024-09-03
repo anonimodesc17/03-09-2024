04befaa4826e74c8e0df1a9ba731ea6aae43cb8d
anonimodesc17 <127567726+anonimodesc17@users.noreply.github.com>
2024-09-03 23:04:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Djonathan";
+const sobreNome = "Oliveira";
+const idade = 18;
+const cidadeNatal = "Curitiba"
+const altura = 1.77;
+
+console.log("O meu primeiro nome é: ");
+console.log(primeiroNome);
+console.log("O meu sobrenome é: ");
+console.log(sobreNome);
+console.log("A minha idade é: ");
+console.log(idade);
+console.log("Minha cidade natal é: ");
+console.log(cidadeNatal);
+console.log("Minha altura é: ");
+console.log(altura);
